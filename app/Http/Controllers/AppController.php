580b824bb7f3507dae7fba8d10216c71ab1c2f7c
Cosmeak <guillaumefine09@gmail.com>
2022-09-27 22:24:41 +0200

    :heavy_plus_sign: ADD fontawesome

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AppController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Home');
+    }
+}
