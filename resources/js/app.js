8ecc25776ec337e933c855fc9e74a60c8edd13a4
Cosmeak <guillaumefine09@gmail.com>
2022-10-01 22:22:21 +0200

    :heavy_plus_sign: ADD moment and select2 dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,13 +6,15 @@ import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import moment from 'moment'
 
 // FontAwesome Livrary
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope, faMapPin, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faMapPin, faPhone, faPowerOff } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFacebookSquare, faGithubSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
-library.add(faEnvelope, faMapPin, faPhone, faGithubSquare, faFacebookSquare, faInstagram)
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-regular-svg-icons'
+library.add(faEnvelope, faMapPin, faPhone, faGithubSquare, faFacebookSquare, faInstagram, faPenToSquare, faTrashCan, faPowerOff)
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -23,9 +25,18 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .component('inertia-link', Link)
+            .component('inertia-head', Head)
             .component('font-awesome-icon', FontAwesomeIcon)
+            .mixin({
+                methods: {
+                    formatDate(date, format) {
+                        return moment(date).format(format);
+                    },
+                }
+            })
             .mount(el);
     },
 });
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#F5A11A' });
