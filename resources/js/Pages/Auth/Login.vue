334da880103eb5a3a358e229bc1cee5350be253d
Cosmeak <guillaumefine09@gmail.com>
2022-11-23 18:12:19 +0000

    :bug: FIX login and favicon

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -27,79 +27,84 @@ const submit = () => {
 <template>
 	<Head title="Connexion" />
 
-	<div
-		v-if="status"
-		class="mb-4 font-medium text-sm text-green-600"
-	>
-		{{ status }}
-	</div>
-
-	<form @submit.prevent="submit">
-		<div>
-			<InputLabel
-				for="email"
-				value="Email"
-			/>
-			<TextInput
-				id="email"
-				v-model="form.email"
-				type="email"
-				class="mt-1 block w-full"
-				required
-				autofocus
-				autocomplete="username"
-			/>
-			<InputError
-				class="mt-2"
-				:message="form.errors.email"
-			/>
+	<div class="w-screen h-screen flex flex-col justify-center items-center">
+		<div
+			v-if="status"
+			class="mb-4 font-medium text-sm text-green-600"
+		>
+			{{ status }}
 		</div>
 
-		<div class="mt-4">
-			<InputLabel
-				for="password"
-				value="Password"
-			/>
-			<TextInput
-				id="password"
-				v-model="form.password"
-				type="password"
-				class="mt-1 block w-full"
-				required
-				autocomplete="current-password"
-			/>
-			<InputError
-				class="mt-2"
-				:message="form.errors.password"
-			/>
-		</div>
-
-		<div class="block mt-4">
-			<label class="flex items-center">
-				<Checkbox
-					v-model:checked="form.remember"
-					name="remember"
+		<form
+			class="w-2/6 bg-white p-8 rounded-lg shadow-lg"
+			@submit.prevent="submit"
+		>
+			<div>
+				<InputLabel
+					for="email"
+					value="Email"
 				/>
-				<span class="ml-2 text-sm text-gray-600">Remember me</span>
-			</label>
-		</div>
+				<TextInput
+					id="email"
+					v-model="form.email"
+					type="email"
+					class="mt-1 block w-full"
+					required
+					autofocus
+					autocomplete="username"
+				/>
+				<InputError
+					class="mt-2"
+					:message="form.errors.email"
+				/>
+			</div>
 
-		<div class="flex items-center justify-end mt-4">
-			<Link
-				v-if="canResetPassword"
-				:href="route('password.request')"
-				class="underline text-sm text-gray-600 hover:text-gray-900"
-			>
-				Mot de passe oublié ?
-			</Link>
+			<div class="mt-4">
+				<InputLabel
+					for="password"
+					value="Password"
+				/>
+				<TextInput
+					id="password"
+					v-model="form.password"
+					type="password"
+					class="mt-1 block w-full"
+					required
+					autocomplete="current-password"
+				/>
+				<InputError
+					class="mt-2"
+					:message="form.errors.password"
+				/>
+			</div>
 
-			<PrimaryButton
-				class="ml-4"
-				:class="{ 'opacity-25': form.processing }"
-				:disabled="form.processing"
-			>
-				Connexion
-			</PrimaryButton>
-		</div>
-	</form>
+			<div class="block mt-4">
+				<label class="flex items-center">
+					<Checkbox
+						v-model:checked="form.remember"
+						name="remember"
+					/>
+					<span class="ml-2 text-sm text-gray-600">Remember me</span>
+				</label>
+			</div>
+
+			<div class="flex items-center justify-end mt-4">
+				<Link
+					v-if="canResetPassword"
+					:href="route('password.request')"
+					class="underline text-sm text-gray-600 hover:text-gray-900"
+				>
+					Mot de passe oublié ?
+				</Link>
+
+				<PrimaryButton
+					class="ml-4"
+					:class="{ 'opacity-25': form.processing }"
+					:disabled="form.processing"
+				>
+					Connexion
+				</PrimaryButton>
+			</div>
+		</form>
+    </div>
 </template>
